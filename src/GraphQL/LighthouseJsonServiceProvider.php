9eb2bdb0a6751b4f0e5f95275a37a3c7625dc728
Thomas Venturini <thomas@du.digital>
2020-08-11 10:40:07 +0200

    add service provider

---
+++ src/GraphQL/LighthouseJsonServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Marqant\LighthouseJosn;
+
+use Illuminate\Support\ServiceProvider;
+
+/** @package Marqant\LighthouseJosn */
+class LighthouseJosnServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerScalars();
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // boot package
+    }
+
+    /**
+     * Register scalars of this package.
+     *
+     * @return void
+     */
+    private function registerScalars(): void
+    {
+        config(['lighthouse.namespaces.scalars' => array_merge((array) config('lighthouse.namespaces.scalars'), (array) 'Marqant\\LighthouseJson\\GraphQL\\Scalars')]);
+    }
+}
